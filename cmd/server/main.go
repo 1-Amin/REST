181d0dd29aa014ef7cae7667a404ff787a85e835
1-Amin <azimovaminjon063@gmail.com>
2026-06-07 12:25:34 +0700

    Added Gin handler

---
+++ cmd/server/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/healthz", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status": "ok",
+			"time":   time.Now().UTC().Format(time.RFC3339),
+		})
+
+	})
+	r.Run(":8080")
+}
